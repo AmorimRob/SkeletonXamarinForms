c178bbd8f3ace22c4748216d14b3f9fbd463f03d
Robson Amorim <robson.amorim@lambda3.com.br>
2020-05-14 00:04:01 -0300

    Initial commit

---
+++ SkeletonScreen/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MvvmHelpers;
+using Xamarin.Forms;
+using Xamarin.Forms.StateSquid;
+
+namespace SkeletonScreen
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+		public State SkeletonState { get; set; }
+		public ICommand SkeletonCommand { get; set; }
+
+		public MainPageViewModel()
+		{
+			SkeletonCommand = new Command(async (x) =>
+			{
+				SkeletonState = State.Loading;
+				await Task.Delay(3000);
+				SkeletonState = State.None;
+			});
+		}
+	}
+}
